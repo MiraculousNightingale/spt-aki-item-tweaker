700760b715ab9f7019353ee3c91d45db3403dbf8
Nightingale <esteticwence@gmail.com>
2023-03-15 00:58:55 +0200

    Implement nested property getter and setter

--- src/applicator.ts
+++ src/applicator.ts
@@ -25,7 +25,7 @@ enum ApplicatorChangeType
  * 
  * Requires a VerboseLogger to optionally output messages based on a config value.
  * 
- * Version 230309
+ * Version 230315
  */
 class Applicator 
 {
@@ -47,24 +47,34 @@ class Applicator
     public canApplyValue(targetObj: object, sourceObj: object, parameter: string, showLogMessages = false): boolean
     {
         // Nothing is written into target object so assign it's property a simpler variable name.
-        const oldValue = targetObj[parameter];
-        const newValue = sourceObj[parameter];
-        if (oldValue !== undefined) 
+        try 
         {
-            if (typeof oldValue !== typeof newValue)
+            // const oldValue = targetObj[parameter];
+            const oldValue = Applicator.getNestedProperty(targetObj, parameter);
+            const newValue = sourceObj[parameter];
+            if (oldValue !== undefined) 
+            {
+                if (typeof oldValue !== typeof newValue)
+                {
+                    if (showLogMessages)
+                        this.logger.error(`[ERROR] "${parameter}": new value type "${typeof newValue}" doesn't match old value type "${typeof oldValue}"!`);
+                    return false;
+                }
+            }
+            else 
             {
                 if (showLogMessages)
-                    this.logger.error(`[ERROR] "${parameter}": new value type "${typeof newValue}" doesn't match old value type "${typeof oldValue}"!`);
+                    this.logger.warning(`[WARNING] "${parameter}" property is undefined in the target object.`);
                 return false;
             }
+            return true;
         }
-        else 
+        catch (error) 
         {
             if (showLogMessages)
-                this.logger.warning(`[WARNING] "${parameter}" property is undefined in the target object.`);
+                this.logger.error(`[ERROR] "${parameter}": ${error}`);
             return false;
         }
-        return true;
     }
 
     /**
@@ -77,31 +87,41 @@ class Applicator
      */
     public canApplyMultiplier(targetObj: object, sourceObj: object, parameter: string, showLogMessages = false): boolean 
     {
+        try 
+        {
         // Nothing is written into target object so assign it's property a simpler variable name.
-        const oldValue = targetObj[parameter];
-        const multiplier = sourceObj[parameter];
-        if (typeof multiplier !== "number" || Number.isNaN(multiplier))
-        {
-            if (showLogMessages)
-                this.logger.error(`[ERROR] "${parameter}" property multiplier has to be a number!`);
-            return false;
-        }
-        if (oldValue !== undefined) 
-        {
-            if (typeof oldValue !== "number" || Number.isNaN(oldValue))
+        // const oldValue = targetObj[parameter];
+            const oldValue = Applicator.getNestedProperty(targetObj, parameter);
+            const multiplier = sourceObj[parameter];
+            if (typeof multiplier !== "number" || Number.isNaN(multiplier))
             {
                 if (showLogMessages)
-                    this.logger.error(`[ERROR] Type of property "${parameter}" is ${typeof oldValue} and can't be multiplied!`);
+                    this.logger.error(`[ERROR] "${parameter}" property multiplier has to be a number!`);
                 return false;
             }
+            if (oldValue !== undefined) 
+            {
+                if (typeof oldValue !== "number" || Number.isNaN(oldValue))
+                {
+                    if (showLogMessages)
+                        this.logger.error(`[ERROR] Type of property "${parameter}" is ${typeof oldValue} and can't be multiplied!`);
+                    return false;
+                }
+            }
+            else 
+            {
+                if (showLogMessages)
+                    this.logger.warning(`[WARNING] "${parameter}" property is undefined in the target object.`);
+                return false;
+            }
+            return true;
         }
-        else 
+        catch (error) 
         {
             if (showLogMessages)
-                this.logger.warning(`[WARNING] "${parameter}" property is undefined in the target object.`);
+                this.logger.error(`[ERROR] "${parameter}": ${error}`);
             return false;
         }
-        return true;
     }
 
     /**
@@ -143,17 +163,18 @@ class Applicator
         if (this.canApplyValue(targetObj, sourceObj, parameter, true))
         {
             const newValue = sourceObj[parameter];
-            const oldValue = targetObj[parameter];
-            if (targetObj[parameter] !== newValue)
+            const oldValue = Applicator.getNestedProperty(targetObj, parameter);
+            if (oldValue !== newValue)
             {
-                targetObj[parameter] = newValue;
+                // targetObj[parameter] = newValue;
+                Applicator.setNestedProperty(targetObj, parameter, newValue);
                 switch (logFormat)
                 {
                     case ApplicatorLogFormat.LIST_ENTRY:
-                        this.logger.success(`├ ${parameter}: Successfully applied value ${JSON.stringify(targetObj[parameter])} (was ${JSON.stringify(oldValue)})`);
+                        this.logger.success(`├ ${parameter}: Successfully applied value ${JSON.stringify(Applicator.getNestedProperty(targetObj, parameter))} (was ${JSON.stringify(oldValue)})`);
                         break;
                     default:
-                        this.logger.success(`"${parameter}": Successfully applied value ${JSON.stringify(targetObj[parameter])} (was ${JSON.stringify(oldValue)})`);
+                        this.logger.success(`"${parameter}": Successfully applied value ${JSON.stringify(Applicator.getNestedProperty(targetObj, parameter))} (was ${JSON.stringify(oldValue)})`);
                         break;
                 }
                 return 1;
@@ -204,19 +225,22 @@ class Applicator
         if (this.canApplyMultiplier(targetObj, sourceObj, parameter, true))
         {
             const multiplier = sourceObj[parameter];
-            const oldValue = targetObj[parameter];
-            targetObj[parameter] *= multiplier;
-            if (oldValue !== targetObj[parameter]) 
+            // const oldValue = targetObj[parameter];
+            const oldValue = Applicator.getNestedProperty(targetObj, parameter);
+            // targetObj[parameter] *= multiplier;
+            Applicator.setNestedProperty(targetObj, parameter, oldValue * multiplier);
+            // if (oldValue !== targetObj[parameter]) 
+            if (oldValue !== Applicator.getNestedProperty(targetObj, parameter)) 
             {
                 switch (logFormat)
                 {
                     case ApplicatorLogFormat.LIST_ENTRY:
                     // Format string seems not that useful here at all, keep for reference or remove later.
                     // this.logger.success("├ %0s: Successfully multiplied by %0s (Before: %0s | After: %0s)", parameter, multiplier, oldValue, targetObj[parameter]);
-                        this.logger.success(`├ ${parameter}: Successfully multiplied by ${multiplier} (Before: ${oldValue} | After: ${targetObj[parameter]})`);
+                        this.logger.success(`├ ${parameter}: Successfully multiplied by ${multiplier} (Before: ${oldValue} | After: ${Applicator.getNestedProperty(targetObj, parameter)})`);
                         break;
                     default:
-                        this.logger.success(`"${parameter}": Successfully multiplied by ${multiplier} (Before: ${oldValue} | After: ${targetObj[parameter]})`);
+                        this.logger.success(`"${parameter}": Successfully multiplied by ${multiplier} (Before: ${oldValue} | After: ${Applicator.getNestedProperty(targetObj, parameter)})`);
                         break;
                 }
                 return 1;
@@ -365,6 +389,30 @@ class Applicator
         return changeCounter;
     }
 
+    public static getNestedProperty(targetObj: object, propertyPath: string): any
+    {
+        if (typeof targetObj !== "object") throw `Trying to get nested property "${propertyPath}": targetObj is not an object. Check property path!`;
+        if (typeof propertyPath !== "string") throw `Trying to get nested property "${propertyPath}": propertyPath is not a string`;
+        
+        // Replace [] notation with dot notation
+        //propertyPath = propertyPath.replace(/\[["'`](.*)["'`]\]/g,".$1")
+
+        const [head, ...rest] = propertyPath.split(".");
+
+        if (!rest.length) return targetObj[head];
+        else return this.getNestedProperty(targetObj[head], rest.join("."));
+    }
+
+    public static setNestedProperty(targetObj: object, propertyPath: string, value: any): void
+    {
+        if (typeof targetObj !== "object") throw `Trying to get nested property "${propertyPath}": targetObj is not an object. Check property path!`;
+        if (typeof propertyPath !== "string") throw `Trying to get nested property "${propertyPath}": propertyPath is not a string`;
+        
+        const [head, ...rest] = propertyPath.split(".");
+
+        if (!rest.length) targetObj[head] = value;
+        else this.setNestedProperty(targetObj[head], rest.join("."), value);
+    }
     
 }
 
